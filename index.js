38462847ee8c9dc9c8e01ac3b2478996a43ea7b7
singhsumitt <130314108+singhsumitt@users.noreply.github.com>
2023-09-22 01:16:42 +0530

    lol

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var randomNo1 =  Math.floor(Math.random()*6)+1;
+
+
+var randomDice1 = "dice" +randomNo1 + ".png";
+var randomImage1 = "images/" +randomDice1;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImage1);
+
+var randomNo2 =  Math.floor(Math.random()*6)+1;
+
+
+var randomDice2 = "dice" +randomNo2 + ".png";
+var randomImage2 = "images/" +randomDice2;
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomImage2);
+
+if(randomDice1>randomDice2){
+   document.querySelector("h1").innerHTML = "player1 Wins" ;
+}
+else if (randomDice1<randomDice2){
+    document.querySelector("h1").innerHTML= "player 2 Lost";
+}
+else{
+    document.querySelector("h1").innerHTML="LOL";
+}
